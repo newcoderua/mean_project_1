9ede7f033d8ec2bb2834c19442dda646ccfde949
vladyslav <tradestadnyk@gmail.com>
2018-02-11 20:44:56 -0500

    Added mongoose and morgan logger

---
+++ roadmap.md
@@ -0,0 +1,7 @@
+1. folder -> server.sj -> npm init
+2. npm install express --save
+3. npm install nodemon --save, then use 'rs' to quickly restart server
+4. npm install morgan --save. It is a middleware (logger), app.use() -> that's how we use all middlewares in express apps.
+5. npm install mongoose --save, handler mongodb for express
+// make sure that you kill :27017 for mongod before running it again
+6.  
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost:27017/mean1');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("we're connected");
+});
+
+app.listen(port, () => {
+  console.log('running the server on port', + port);
+});
